99ff590335d72287aa554d92b934fb9d5b38408c
Naman <60322358+namanzzz@users.noreply.github.com>
2023-09-06 07:43:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import {getStorage} from 'firebase/storage'
 
-
+// Please Refer firebase console for firebaseConfig object
 const firebaseConfig = {
-    apiKey: "AIzaSyD3GwteX5kcCSWfN9UYutX652rOujJDARc",
-    authDomain: "social-chat-c6c51.firebaseapp.com",
-    projectId: "social-chat-c6c51",
-    storageBucket: "social-chat-c6c51.appspot.com",
-    messagingSenderId: "64304277622",
-    appId: "1:64304277622:web:f38c0a4b5a660aac325757"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-
+export const storage = getStorage();
+export const db = getFirestore();
